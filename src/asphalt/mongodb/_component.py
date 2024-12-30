60649f0040c23c8c0a931d35e97c8bf5e120ec0b
Alex Grönholm <alex.gronholm@nextday.fi>
2024-12-31 00:19:08 +0200

    Removed support for the resource_name parameter

--- src/asphalt/mongodb/_component.py
+++ src/asphalt/mongodb/_component.py
@@ -1,13 +1,10 @@
 from __future__ import annotations
 
-import logging
 from collections.abc import Mapping
 from typing import Any
 
 from asphalt.core import Component, add_resource
 from motor.motor_asyncio import AsyncIOMotorClient
-
-logger = logging.getLogger("asphalt.mongodb")
 
 
 class MongoDBComponent(Component):
@@ -19,33 +16,15 @@ class MongoDBComponent(Component):
 
     :param client_args: a dictionary of keyword arguments to pass to
         :class:`~motor.motor_asyncio.AsyncIOMotorClient`
-    :param resource_name: default values for omitted
-        :class:`~motor.motor_asyncio.AsyncIOMotorClient` arguments
     """
 
-    def __init__(
-        self,
-        *,
-        client_args: Mapping[str, Any] | None = None,
-        resource_name: str = "default",
-    ):
+    def __init__(self, *, client_args: Mapping[str, Any] | None = None):
         options = client_args or {}
         self._client = AsyncIOMotorClient(**options)
-        self.resource_name = resource_name
 
     async def start(self) -> None:
         add_resource(
             self._client,
-            self.resource_name,
             description="MongoDB client",
             teardown_callback=self._client.close,
-        )
-        hosts = {
-            f"{host}:{port}"
-            for host, port in self._client.topology_description.server_descriptions()
-        }
-        logger.info(
-            "Configured MongoDB client (%s; hosts=%r)",
-            self.resource_name,
-            hosts,
         )
--- tests/test_component.py
+++ tests/test_component.py
@@ -1,10 +1,8 @@
-import logging
 import os
 
 import pytest
 from asphalt.core import Context, get_resource_nowait
 from motor.motor_asyncio import AsyncIOMotorClient
-from pytest import LogCaptureFixture
 
 from asphalt.mongodb import MongoDBComponent
 
@@ -13,14 +11,8 @@ MONGODB_HOSTNAME = os.getenv("MONGODB_HOST", "localhost")
 pytestmark = pytest.mark.anyio
 
 
-async def test_default_client(caplog: LogCaptureFixture) -> None:
+async def test_default_client() -> None:
     """Test that the client is created and is available on the context."""
-    caplog.set_level(logging.INFO, "asphalt.mongodb")
     async with Context():
         await MongoDBComponent().start()
         get_resource_nowait(AsyncIOMotorClient)
-
-    assert len(caplog.messages) == 1
-    assert caplog.messages == [
-        "Configured MongoDB client (default; hosts={'localhost:27017'})"
-    ]
